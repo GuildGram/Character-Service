7599c77e720014bcf2be8d9d4c8e47ba49fa2134
MCCello <86051935+MCCello@users.noreply.github.com>
2022-04-06 17:09:18 +0200

    Started implementing rest

--- main.go
+++ main.go
@@ -1,20 +1,46 @@
 package main
 
 import (
+	"context"
 	"log"
 	"net/http"
 	"os"
+	"os/signal"
+	"time"
 
 	"github.com/GuildGram/Character-Service.git/handlers"
 )
 
 func main() {
 	l := log.New(os.Stdout, "character-api", log.LstdFlags)
-	ch := handlers.NewCreate(l)
+
+	ch := handlers.NewCharacter(l)
 
 	sm := http.NewServeMux()
 	sm.Handle("/", ch)
 
-	// http.HandleFunc()
-	http.ListenAndServe(":9090", nil)
+	s := &http.Server{
+		Addr:         ":9090",
+		Handler:      sm,
+		IdleTimeout:  120 * time.Second,
+		ReadTimeout:  1 * time.Second,
+		WriteTimeout: 1 * time.Second,
+	}
+
+	go func() {
+		err := s.ListenAndServe()
+		if err != nil {
+			l.Fatal(err)
+		}
+	}()
+
+	sigChan := make(chan os.Signal)
+	signal.Notify(sigChan, os.Interrupt)
+	signal.Notify(sigChan, os.Kill)
+
+	sig := <-sigChan
+	l.Println("received kill signal, shutting down", sig)
+
+	tc, _ := context.WithTimeout(context.Background(), 30*time.Second)
+	s.Shutdown(tc)
 }
