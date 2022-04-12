a4a3085490788f1e5d860ed4e1017f03cf660885
MCCello <86051935+MCCello@users.noreply.github.com>
2022-04-12 12:14:02 +0200

    Update main.go

--- main.go
+++ main.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+	"context"
 	"log"
 	"net/http"
 	"os"
+	"os/signal"
+	"time"
 
 	"github.com/GuildGram/Character-Service.git/handlers"
 	"github.com/gorilla/mux"
@@ -35,28 +38,28 @@ func main() {
 	deleteRouter.HandleFunc("/characters/delete{id:[0-9]+}", ch.DeleteCharacter)
 
 	//Server stuff for testing, will be deleted soon
-	// s := &http.Server{
-	// 	Addr:         ":9090",
-	// 	Handler:      sm,
-	// 	IdleTimeout:  120 * time.Second,
-	// 	ReadTimeout:  1 * time.Second,
-	// 	WriteTimeout: 1 * time.Second,
-	// }
+	s := &http.Server{
+		Addr:         ":9090",
+		Handler:      sm,
+		IdleTimeout:  120 * time.Second,
+		ReadTimeout:  1 * time.Second,
+		WriteTimeout: 1 * time.Second,
+	}
 
-	// go func() {
-	// 	err := s.ListenAndServe()
-	// 	if err != nil {
-	// 		l.Fatal(err)
-	// 	}
-	// }()
+	go func() {
+		err := s.ListenAndServe()
+		if err != nil {
+			l.Fatal(err)
+		}
+	}()
 
-	// sigChan := make(chan os.Signal)
-	// signal.Notify(sigChan, os.Interrupt)
-	// signal.Notify(sigChan, os.Kill)
+	sigChan := make(chan os.Signal)
+	signal.Notify(sigChan, os.Interrupt)
+	signal.Notify(sigChan, os.Kill)
 
-	// sig := <-sigChan
-	// l.Println("received kill signal, shutting down", sig)
+	sig := <-sigChan
+	l.Println("received kill signal, shutting down", sig)
 
-	// tc, _ := context.WithTimeout(context.Background(), 30*time.Second)
-	// s.Shutdown(tc)
+	tc, _ := context.WithTimeout(context.Background(), 30*time.Second)
+	s.Shutdown(tc)
 }
